d019435953fc6dad04d3e8c5b78ec82356afcaa6
hannutho <mail@maximiliankeller.com>
2024-07-21 15:46:49 +0200

    asseturl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "" : undefined;
-const assetPrefix = isProd ? "" : undefined;
+const assetPrefix = isProd ? "chrome-dino" : undefined;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
