abd77e7e09f52c4d9d90b00ce3f39b67bb4d7625
hannutho <mail@maximiliankeller.com>
2024-07-21 15:44:24 +0200

    vf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/" : undefined;
-const assetPrefix = isProd ? "/" : undefined;
+const basePath = isProd ? "" : undefined;
+const assetPrefix = isProd ? "" : undefined;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
